0bae0a7bbf65150a13a247763f4e234a66813eeb
Youri <youri@vanderstar.eu>
2022-09-26 15:29:57 +0200

    Dag 12: Rode piste

--- Dag12/LINQ.Test/LINQTest.cs
+++ Dag12/LINQ.Test/LINQTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -7,18 +8,18 @@ namespace LINQ.Test;
 [TestClass]
 public class LINQTest
 {
+    static readonly List<string> KlasDomainExpertCase = new List<string>
+    {
+        "Dennis", "Mark", "Ruben", "Milan", "Xander", "Arjen",
+        "Maurice", "Stijn", "Raymon", "Lex", "Jort",
+        "Melissa", "Erik", "Youri", "Wouter", "Wesley",
+        "Wouter", "Jasper", "Jan", "Djurre", "Luuk", "Tim"
+    };
+    
     [TestMethod]
     public void FirstLettersOfEachStudentThatHasAnJInThouName_Comprehension()
     {
-        List<string> klas = new List<string>
-        {
-            "Dennis", "Mark", "Ruben", "Milan", "Xander", "Arjen",
-            "Maurice", "Stijn", "Raymon", "Lex", "Jort",
-            "Melissa", "Erik", "Youri", "Wouter", "Wesley",
-            "Wouter", "Jasper", "Jan", "Djurre", "Luuk"
-        };
-
-        var result = LINQKlas.FindWithJ_comperhension(klas);
+        var result = LINQKlas.FindWithJ_comperhension(KlasDomainExpertCase);
 
         var expected = new List<char> { 'A', 'S', 'J', 'J', 'J', 'D' };
         CollectionAssert.AreEqual(expected, result);
@@ -44,15 +45,7 @@ public class LINQTest
     [TestMethod]
     public void LengthOfNameForEachStudentThatStartWithLetterM_Method()
     {
-        List<string> klas = new List<string>
-        {
-            "Dennis", "Mark", "Ruben", "Milan", "Xander", "Arjen",
-            "Maurice", "Stijn", "Raymon", "Lex", "Jort",
-            "Melissa", "Erik", "Youri", "Wouter", "Wesley",
-            "Wouter", "Jasper", "Jan", "Djurre", "Luuk"
-        };
-
-        var result = LINQKlas.FindLengthStartingWithMDescending_method(klas);
+        var result = LINQKlas.FindLengthStartingWithMDescending_method(KlasDomainExpertCase);
 
         var expected = new List<int> { 7, 7, 5, 4 };
         CollectionAssert.AreEqual(expected, result);
@@ -62,17 +55,73 @@ public class LINQTest
     [TestMethod]
     public void LengthOfNameForEachStudentThatStartWithLetterM_Comprehension()
     {
-        List<string> klas = new List<string>
-        {
-            "Dennis", "Mark", "Ruben", "Milan", "Xander", "Arjen",
-            "Maurice", "Stijn", "Raymon", "Lex", "Jort",
-            "Melissa", "Erik", "Youri", "Wouter", "Wesley",
-            "Wouter", "Jasper", "Jan", "Djurre", "Luuk"
-        };
-
-        var result = LINQKlas.FindLengthStartingWithMDescending_comperhension(klas);
+        var result = LINQKlas.FindLengthStartingWithMDescending_comperhension(KlasDomainExpertCase);
 
         var expected = new List<int> { 7, 7, 5, 4 };
+        CollectionAssert.AreEqual(expected, result);
+    }
+    
+    [TestMethod]
+    public void CountOfNameLengthByLength_comperhension()
+    {
+        List<string> sut = new List<string>
+        {
+            "Twee", "Een", "Drie", "Vier", "Vijf", "Zes"
+        };
+
+        var result = LINQKlas.CountNameLengthByName_comperhension(sut);
+        
+        var expected = new List<int> { 2, 4 };
+        CollectionAssert.AreEqual(expected, result);
+    }
+    
+    [TestMethod]
+    public void CountOfNameLengthByLength_method()
+    {
+        List<string> sut = new List<string>
+        {
+            "Twee", "Een", "Zes"
+        };
+
+        var result = LINQKlas.CountNameLengthByName_method(sut);
+        
+        var expected = new List<int> { 2, 1 };
+        CollectionAssert.AreEqual(expected, result);
+    }
+    
+    [TestMethod]
+    public void CountOfNameLengthByLength_method_DomainCases()
+    {
+        var result = LINQKlas.CountNameLengthByName_method(KlasDomainExpertCase);
+        
+        var expected = new List<int> { 3, 4, 5, 8, 2 };
+        CollectionAssert.AreEqual(expected, result);
+    }
+    
+    [TestMethod]
+    public void CountOfNameLengthByLength_comprehension_DomainCases()
+    {
+        var result = LINQKlas.CountNameLengthByName_comperhension(KlasDomainExpertCase);
+        
+        var expected = new List<int> { 3, 4, 5, 8, 2 };
+        CollectionAssert.AreEqual(expected, result);
+    }
+    
+    [TestMethod] 
+    public void ThreeLetterNames_WithM_comprehension_DomainCases()
+    {
+        var result = LINQKlas.NamesShorterThanWithoutLetter_comprehension(KlasDomainExpertCase, 'm');
+        
+        var expected = new List<string> { "Jan", "Lex" };
+        CollectionAssert.AreEqual(expected, result);
+    }
+    
+    [TestMethod] 
+    public void ThreeLetterNames_WithM_method_DomainCases()
+    {
+        var result = LINQKlas.NamesShorterThanWithoutLetter_method(KlasDomainExpertCase, 'm');
+        
+        var expected = new List<string> { "Jan", "Lex" };
         CollectionAssert.AreEqual(expected, result);
     }
 }
--- Dag12/LINQ/LINQKlas.cs
+++ Dag12/LINQ/LINQKlas.cs
@@ -42,4 +42,52 @@ public class LINQKlas
                 
         return query.ToList();
     }
+
+    public static List<int> CountNameLengthByName_comperhension(List<string> list)
+    {
+        var query =
+            from naam in list
+            let naamLength = naam.Length
+            orderby naamLength
+            group naam by naamLength
+            into lengthGroup
+            select lengthGroup.Count();
+        
+        return query.ToList();
+    }
+    
+    public static List<int> CountNameLengthByName_method(List<string> list)
+    {
+        var query = list
+            .OrderBy(naam => naam.Length)
+            .GroupBy(naam => naam.Length)
+            .Select(grouping => grouping.Count());
+        return query.ToList();
+    }
+    
+    public static List<string> NamesShorterThanWithoutLetter_comprehension(List<string> list, char withoutLetter = 'm')
+    {
+        var query =
+            from naam in list
+            let maxLenght = (from subNaam in list
+                select subNaam.Length).Min()
+            orderby naam
+            where naam.Length <= maxLenght
+            where !naam.Contains(withoutLetter, StringComparison.InvariantCultureIgnoreCase)
+            select naam;
+
+        return query.ToList();
+    }
+    
+    public static List<string> NamesShorterThanWithoutLetter_method(List<string> list, char withoutLetter = 'm')
+    {
+        var query = list
+            .Select(naam => naam)
+            .OrderBy(naam => naam)
+            .Where(naam => naam.Length <= list.Min(naam => naam.Length))
+            .Where(naam => !naam.Contains(withoutLetter, StringComparison.InvariantCultureIgnoreCase));
+
+        return query.ToList();
+    }
+    
 }
