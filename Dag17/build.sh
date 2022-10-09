32fb18d58c0e3c364f508fe5afee6b77d8f62e60
Youri <youri@vanderstar.eu>
2022-10-09 17:23:34 +0200

    Updated docker-compose and added Docker build en push scripts.

---
+++ Dag17/build.sh
@@ -0,0 +1,2 @@
+docker build -t huminor.azurecr.io/yvds-dag20-orderbackend:latest Backend/OrderBackend/.
+docker build -t huminor.azurecr.io/yvds-dag20-orderwebsite:latest Frontend/OrderWebsite/.
---
+++ Dag17/push.sh
@@ -0,0 +1,2 @@
+docker push huminor.azurecr.io/yvds-dag20-orderbackend:latest
+docker push huminor.azurecr.io/yvds-dag20-orderwebsite:latest
