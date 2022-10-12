e6209a5fc648836d5c72608afa02b8b8eea73524
Youri <youri@vanderstar.eu>
2022-10-12 16:02:00 +0200

    CAS: Seperated FileUpload into In- and OutFileUpload on BE.

--- CASYvdS/backend/CASbackend/Models/OutFileUpload.cs
+++ CASYvdS/backend/CASbackend/Models/OutFileUpload.cs
@@ -1,11 +1,10 @@
 namespace CASbackend.Models;
 
-public record FileUpload
+public record OutFileUpload
 {
     public bool IsValid { get; set; }
     
     public bool IsValidated { get; set; } = true;
-    
     public byte[] Content { get; set; }
     public int Duplicates { get; set; }
     public int CursusInserts { get; set; }
