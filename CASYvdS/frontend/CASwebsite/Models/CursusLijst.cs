77e9564c89ba70c75d0e475ad482b591647b185b
Youri <youri@vanderstar.eu>
2022-10-12 16:03:59 +0200

    CAS: Added week navigation buttons on FE

--- CASYvdS/frontend/CASwebsite/Models/CursusLijst.cs
+++ CASYvdS/frontend/CASwebsite/Models/CursusLijst.cs
@@ -1,10 +1,14 @@
-using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 namespace CASwebsite.Models;
 
 public class CursusLijst
 {
     public int Weeknummer { get; set; }
+    
+    public int VorigeWeek { get; set; }
+    
+    public int VolgendeWeek { get; set; }
     public IEnumerable<CursusInstantie> CursusInstanties { get; set; }
 
     public CursusLijst()
@@ -13,6 +17,9 @@ public class CursusLijst
 
     public CursusLijst(int weeknummer, IEnumerable<CursusInstantie> cursusInstanties)
     {
+        DateTime mondayOfCurrentWeek = ISOWeek.ToDateTime(DateTime.Today.Year, weeknummer, DayOfWeek.Monday);
+        VorigeWeek = ISOWeek.GetWeekOfYear(mondayOfCurrentWeek.AddDays(-7));
+        VolgendeWeek = ISOWeek.GetWeekOfYear(mondayOfCurrentWeek.AddDays(7));
         Weeknummer = weeknummer;
         CursusInstanties = cursusInstanties;
     }
